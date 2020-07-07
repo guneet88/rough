31d479665592a596393366b0d872f92f744c32ea
Guneet <masterguneet@gmail.com>
2020-07-07 14:35:05 +0530

    adding all files

---
+++ script.js
@@ -0,0 +1,19 @@
+function mini() {
+    document.getElementById("mini2222").innerHTML=Date();
+}
+
+
+
+
+function add() {
+
+x = 5;
+y = 10;
+z = x + y;
+
+document.getElementById('calc').innerHTML=z;
+   
+}
+
+
+
